eb71e44fc417914874a7015d7adf946cfc4b03c8
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-06-26 00:48:00 -0300

    feat: add menu modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
-import React from "react";
-import user from "./User";
+import React, { useState } from "react";
 import { SealCheck, ShareNetwork } from "@phosphor-icons/react";
+import user from "./User";
+import Modal from "./Modal";
+import Menu from"./Menu";
 
 export default function Header() {
+    const [openMenu, setOpenMenu] = useState(false);    
+
+    function toggleModal() {
+        setOpenMenu(!openMenu);
+    } 
+
     return(
-        <header className="">
+        <header>
             <div className="container mx-auto p-6">
                 <div className="flex justify-between items-center p-2 rounded-full bg-white bg-opacity-50 backdrop-blur-md border">
                     <div>
@@ -21,12 +29,18 @@ export default function Header() {
                         <SealCheck size={24} weight="fill" className="text-sky-500" />
                     </div>
                     <div>
-                        <button className="p-2 bg-zinc-900 shadow-sm rounded-full hover:shadow-2xl transition-all">
+                        <button className="p-2 bg-zinc-900 shadow-sm rounded-full hover:shadow-2xl transition-all" onClick={toggleModal}>
                             <ShareNetwork size={24} weight={"fill"} className="text-white" />
                         </button>
                     </div>
                 </div>
             </div>
+            <Modal 
+                title="Menu"
+                content={<Menu />} 
+                isOpen={openMenu} 
+                toggleModal={toggleModal}
+            />
         </header>
     )
 }
